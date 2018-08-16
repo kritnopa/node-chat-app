52c5236c7658eac1b080a4cfb872a06ea7ce50db
Kritnop <kritnop@santoranakama.com>
2018-08-17 01:52:47 +0700

    add form for messages and show incoming in browser

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,28 @@ socket.on('newEmail', function (email) {
 	console.log('New email', email);
 });
 
-socket.on('newMessage', function (email) {
-	console.log('New Message', email);
-})
+socket.on('newMessage', function (message) {
+	console.log('New Message', message);
+	var li = jQuery('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
+
+	jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+// 	from: 'Frank',
+// 	text: 'Hi'
+// }, function (data) { 
+// 	console.log('Got it', data);
+// });
+
+jQuery('#message-form').on('submit', function (e) {
+	e.preventDefault();
+
+	socket.emit('createMessage', {
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	}, function () {
+
+	});
+});
--- server/server.js
+++ server/server.js
@@ -19,10 +19,11 @@ io.on(`connection`, (socket) => {
 
 	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 	
-	socket.on('createMessage', (message) => {
+	socket.on('createMessage', (message, callback) => {
 		console.log('createMessage', message);
 
-		io.emit('newMessage', generateMessage(message.form, message.text));
+		io.emit('newMessage', generateMessage(message.from, message.text));
+		callback('This is the string in the server');
 		// socket.broadcast.emit('newMessage', {
 		// 	from: message.form,
 		// 	text: message.text,
